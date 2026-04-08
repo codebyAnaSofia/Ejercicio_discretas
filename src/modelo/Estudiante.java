53315407bd3a9443bf33cdc769caa0bb172f51f1
codebyAnaSofia <1110046177@u.icesi.edu.co>
2026-04-08 17:30:29 -0500

    Implement Estudiante class with attributes and methods
    Added fields and methods for Estudiante class.

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -1,1 +1,63 @@
+package modelo;
+
+public class Estudiante {
+    private Integer codigo;
+    private String nombre;
+    private String apellido;
+    private String carrera;
+
+    public Estudiante(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Estudiante(Integer codigo, String nombre, String apellido, String carrera) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.carrera = carrera;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+  @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Estudiante)) return false;
+        Estudiante otro = (Estudiante) obj;
+        return this.codigo.equals(otro.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante:codigo " + codigo + ", nombre " + nombre + " " + apellido + ", carrera=" + carrera;
+    }
+}
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
 
