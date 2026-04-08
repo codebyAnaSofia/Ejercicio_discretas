593c4946a10324c18e06ff1c3c38b2e72779d236
codebyAnaSofia <1110046177@u.icesi.edu.co>
2026-04-08 17:35:47 -0500

    Add Producto class with properties and methods

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,1 +1,54 @@
+package modelo;
 
+public class Producto {
+    private String codigo;     
+    private String nombre;
+    private double precio;
+
+    public Producto(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Producto(String codigo, String nombre, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+  
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+  
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Producto)) return false;
+        Producto otro = (Producto) obj;
+        return this.codigo.equals(otro.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto: codigo " + codigo + ", nombre " + nombre + ", precio " + precio;
+    }
+}
