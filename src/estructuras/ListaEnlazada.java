4cfc27c6faa8ecbdb61321f143e27b52ddfe20a5
Jhoan07u <arturomarinmaigual@gmail.com>
2026-04-13 12:27:08 -0500

    Lista enlazada, agregar

--- src/estructuras/ListaEnlazada.java
+++ src/estructuras/ListaEnlazada.java
@@ -1,1 +1,25 @@
+package estructuras;
 
+public class ListaEnlazada<T>{
+    private Nodo<T> cabeza;
+    private int tamano;
+
+    public ListaEnlazada(){
+        cabeza = null;
+        tamano = 0;
+    }
+    public void agregar (T dato){
+        Nodo<T> nuevo = new Nodo<>(dato);
+
+        if (cabeza == null){
+            cabeza = nuevo;
+
+        } else {
+            Nodo<T> actual = cabeza;
+            while(actual.getSiguiente() != null){
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+        }
+    }
+}
