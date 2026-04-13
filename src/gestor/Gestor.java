266325a6d1ccad18e58183a59347f2f28f52cc50
Jhoan07u <arturomarinmaigual@gmail.com>
2026-04-13 13:59:59 -0500

    gestor completado base listaenlazada

--- src/gestor/Gestor.java
+++ src/gestor/Gestor.java
@@ -1,1 +1,34 @@
+package gestor;
+
+import estructuras.ListaEnlazada;
+
+public class Gestor<T>{
+    private  ListaEnlazada<T> lista;
+
+    public Gestor(){
+        lista = new ListaEnlazada<>();
+
+    }
+    //agregar elemento
+    public void agregar(T dato){
+        lista.agregar(dato);
+    }
+    // obtener por indice
+    public T obtener(int index){
+        return lista.obtener(index);
+    }
+    // buscar elemento
+    public T buscar (T dato){
+        return lista.buscar(dato);
+    }
+    //buscar elemento y retornarlo
+    public T eliminar(T dato){
+        return lista.eliminar(dato);
+
+    }
+    //size tamano
+    public int size(){
+        return lista.size();
+    }
+}
 
